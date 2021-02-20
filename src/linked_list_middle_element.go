31953cc7ebb47593b23866c91497f1325ba4045c
MikeWooster <mike.wooster@growthstreet.co.uk>
2021-02-20 21:45:59 +0000

    move linkedlist to separate file

--- src/linked_list_middle_element.go
+++ src/linked_list_middle_element.go
@@ -2,68 +2,7 @@ package main
 
 import (
 	"errors"
-	"fmt"
 )
-
-// Node stores the value of the linked list as an integer.
-type Node struct {
-	value int
-	next  *Node
-}
-
-// LinkedList represents a singly linked list
-// containing integer values.
-type LinkedList struct {
-	head *Node
-	tail *Node
-	len  int
-}
-
-// GetLen returns the current length of the linked list.
-func (l *LinkedList) GetLen() int { return l.len }
-
-// Append adds a single new integer to the linked list.
-func (l *LinkedList) Append(v int) {
-	// Create a new node with no next defined.
-	node := &Node{value: v}
-
-	if l.GetLen() == 0 {
-		// The list is empty, we need to start the head.
-		l.head = node
-	} else {
-		// Get the last element and set it's next value
-		prev := l.tail
-		prev.next = node
-	}
-	// This new node is now the new last position in the list.
-	l.tail = node
-
-	// We can increment the length of the list now.
-	l.len++
-}
-
-// Extend adds a x number of integers to the linked list.
-func (l *LinkedList) Extend(vals []int) {
-	for _, v := range vals {
-		l.Append(v)
-	}
-}
-
-// AtIndex returns the integer stored at position `i` in the linked list.
-func (l *LinkedList) AtIndex(i int) (int, error) {
-	if i >= l.GetLen()-1 {
-		return 0, errors.New("index out of range")
-	}
-	node := l.head
-	for x := 0; x < i; x++ {
-		node = node.next
-		if node == nil {
-			return 0, fmt.Errorf("unexpected error accessing node at position %v", i)
-		}
-	}
-
-	return node.value, nil
-}
 
 func GetLinkedListMiddleElement(l LinkedList) (int, error) {
 	middle := l.GetLen() / 2
@@ -73,7 +12,7 @@ func GetLinkedListMiddleElement(l LinkedList) (int, error) {
 // GetLinkedListMiddleElementNoLen performs the same operation,
 // but assumes the linked list doesnt store the length.
 func GetLinkedListMiddleElementNoLen(l LinkedList) (int, error) {
-	node := l.head
+	node := l.Head
 	if node == nil {
 		return 0, errors.New("index out of range")
 	}
@@ -86,17 +25,17 @@ func GetLinkedListMiddleElementNoLen(l LinkedList) (int, error) {
 	// This means that when you get to the end of the list using
 	// the first pointer, the other pointer will be in the middle.
 	for {
-		node = node.next
+		node = node.Next
 		if node == nil {
 			break
 		}
 
 		if count%2 == 0 {
-			half_node = half_node.next
+			half_node = half_node.Next
 		}
 
 		count++
 	}
 
-	return half_node.value, nil
+	return half_node.Value, nil
 }
---
+++ src/linkedlist.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+// Node stores the Value of the linked list as an integer.
+type Node struct {
+	Value int
+	Next  *Node
+}
+
+// LinkedList represents a singly linked list
+// containing integer values.
+type LinkedList struct {
+	Head *Node
+	Tail *Node
+	len  int
+}
+
+// GetLen returns the current length of the linked list.
+func (l *LinkedList) GetLen() int { return l.len }
+
+// Append adds a single new integer to the linked list.
+func (l *LinkedList) Append(v int) {
+	// Create a new node with no Next defined.
+	node := &Node{Value: v}
+
+	if l.GetLen() == 0 {
+		// The list is empty, we need to start the Head.
+		l.Head = node
+	} else {
+		// Get the last element and set it's Next Value
+		prev := l.Tail
+		prev.Next = node
+	}
+	// This new node is now the new last position in the list.
+	l.Tail = node
+
+	// We can increment the length of the list now.
+	l.len++
+}
+
+// Extend adds a x number of integers to the linked list.
+func (l *LinkedList) Extend(vals []int) {
+	for _, v := range vals {
+		l.Append(v)
+	}
+}
+
+// AtIndex returns the integer stored at position `i` in the linked list.
+func (l *LinkedList) AtIndex(i int) (int, error) {
+	if i >= l.GetLen()-1 {
+		return 0, errors.New("index out of range")
+	}
+	node := l.Head
+	for x := 0; x < i; x++ {
+		node = node.Next
+		if node == nil {
+			return 0, fmt.Errorf("unexpected error accessing node at position %v", i)
+		}
+	}
+
+	return node.Value, nil
+}
